2511cc1914017899e8e4cc7b75940d283b3d9e01
nbear3 <jcbutter@gmail.com>
2016-09-29 22:43:23 -0400

    Fix

---
+++ MouseSwitch/MouseSwitch/Main.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MouseSwitch
+{
+    partial class msMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lightButton = new MetroFramework.Controls.MetroButton();
+            this.darkButton = new MetroFramework.Controls.MetroButton();
+            this.metroStyleManager = new MetroFramework.Components.MetroStyleManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lightButton
+            // 
+            this.lightButton.Location = new System.Drawing.Point(23, 94);
+            this.lightButton.Name = "lightButton";
+            this.lightButton.Size = new System.Drawing.Size(110, 43);
+            this.lightButton.TabIndex = 0;
+            this.lightButton.Text = "Light";
+            this.lightButton.UseSelectable = true;
+            // 
+            // darkButton
+            // 
+            this.darkButton.Location = new System.Drawing.Point(23, 143);
+            this.darkButton.Name = "darkButton";
+            this.darkButton.Size = new System.Drawing.Size(110, 50);
+            this.darkButton.TabIndex = 1;
+            this.darkButton.Text = "Dark";
+            this.darkButton.UseSelectable = true;
+            // 
+            // metroStyleManager
+            // 
+            this.metroStyleManager.Owner = null;
+            this.metroStyleManager.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // msMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.darkButton);
+            this.Controls.Add(this.lightButton);
+            this.Name = "msMain";
+            this.Text = "Mouse Switch";
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton lightButton;
+        private MetroFramework.Controls.MetroButton darkButton;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager;
+    }
+}
+
---
+++ MouseSwitch/MouseSwitch/Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+
+namespace MouseSwitch
+{
+    public partial class msMain : MetroFramework.Forms.MetroForm
+    {
+        public msMain()
+        {
+            InitializeComponent();
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+
+            var usbDevices = GetUSBDevices();
+            foreach (var usbDevice in usbDevices)
+            {
+                Console.WriteLine("Device ID: {0}, PNP Device ID: {1}, Description: {2}",
+                    usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+            }
+
+
+
+            RawInput.RawInputDeviceEnumerator info = new RawInput.RawInputDeviceEnumerator();
+            List<RawInput.RawInputDevice> info_list = info.Devices.ToList();
+
+            for (int i = 0; i < info_list.Count; i++)
+            {
+                RawInput.RawInputDevice device = info_list[i];
+                if (device.DeviceType == RawInput.Win32.RawInputDeviceType.Mouse)
+                {
+                    Console.WriteLine(info_list[i].DeviceInfo.HIDInfo.VendorID);
+                }
+            }
+        }
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+
+    }
+
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+}
